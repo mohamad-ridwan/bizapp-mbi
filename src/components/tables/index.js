7add3a83549536ce3b704a627078b51dfc656a75
mohamad-ridwan <mr643062@gmail.com>
2024-08-14 21:52:39 +0700

    add pagination

--- src/components/tables/index.js
+++ src/components/tables/index.js
@@ -1,8 +1,6 @@
 class Tables extends HTMLElement {
     static get observedAttributes() {
-        return [
-            'thead',
-        ];
+        return [];
     }
     constructor() {
         super()
@@ -17,19 +15,17 @@ class Tables extends HTMLElement {
     }
 
     render() {
-        const thead = this.getAttribute('thead') ?? ''
-
         this.innerHTML = `
-        <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white rounded-xl bg-clip-border">
-        <table class="w-full text-left table-auto min-w-max">
-            <thead>
-                <tr id="title-row">
-                    ${thead}
-                </tr>
-            </thead>
-            <tbody id="table-body">
-            </tbody>
-        </table>
+        <div class="flex w-full bg-white flex-col rounded-bl-xl rounded-br-xl">
+            <div class="relative flex flex-col w-full h-full overflow-scroll text-gray-700 bg-white rounded-tl-xl rounded-tr-xl bg-clip-border">
+            <table class="w-full text-left table-auto min-w-max">
+                <thead>
+                    <tr id="title-row"></tr>
+                </thead>
+            <tbody id="table-body"></tbody>
+            </table>
+            </div>
+            <div id="table-pagination" class="flex justify-center p-4"></div>
         </div>
         `
     }
---
+++ src/components/tables/pagination.js
@@ -0,0 +1,29 @@
+const tablePagination = document.getElementById('table-pagination')
+tablePagination.innerHTML = `
+        <div class="flex items-center gap-8">
+            <button disabled
+            class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg border border-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            type="button">
+            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                aria-hidden="true" class="w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+                </svg>
+            </span>
+            </button>
+            <p class="block font-[axiforma-regular] text-[13px] antialiased font-normal leading-relaxed text-gray-700">
+                Page <strong class="text-gray-900">1</strong> of
+                    <strong class="text-gray-900">10</strong>
+            </p>
+            <button
+                class="relative h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg border border-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button">
+                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+                aria-hidden="true" class="w-4 h-4">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+                </svg>
+                </span>
+                </button>
+        </div>
+`
